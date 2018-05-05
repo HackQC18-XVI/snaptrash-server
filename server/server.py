5b741f2a08ffa81f5fa527b03148b59f6a7ab432
Hans Maryil <hans_maryil@nuance.com>
2018-05-05 14:14:23 -0400

    Add endpoint for photo uploading

--- server/server.py
+++ server/server.py
@@ -1,16 +1,35 @@
 import os
+import tempfile
+
 from flask import Flask
 from flask import jsonify
 from flask import request
 
+FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
+
 app = Flask(__name__)
 
-FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
+
+SUPPORTED_EXTENSIONS = ('.jpg', '.jpeg')
+
+
+def allowed_file(filename):
+    return filename.lower().endswith(SUPPORTED_EXTENSIONS)
 
 
 @app.route("/ping")
 def ping():
     return "pong"
+
+
+@app.route('/photo', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        # get classification from Sultan's classifier
+
+        return results
 
 
 if __name__ == "__main__":
