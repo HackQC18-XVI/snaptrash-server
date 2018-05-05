f1ca01c58948abd3a532b04edb9ca2cba86b1ed1
Hans Maryil <hans_maryil@nuance.com>
2018-05-05 14:27:21 -0400

    Working upload endpoint

--- server/server.py
+++ server/server.py
@@ -5,17 +5,16 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
 
 app = Flask(__name__)
 
+FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
 
-SUPPORTED_EXTENSIONS = ('.jpg', '.jpeg')
+SUPPORTED_EXTENSIONS = ('.png', '.jpg', '.jpeg')
 
 
 def allowed_file(filename):
     return filename.lower().endswith(SUPPORTED_EXTENSIONS)
-
 
 @app.route("/ping")
 def ping():
@@ -25,11 +24,13 @@ def ping():
 @app.route('/photo', methods=['POST'])
 def upload():
     file = request.files['file']
+    results = []
 
     if file and allowed_file(file.filename):
         # get classification from Sultan's classifier
 
-        return results
+        pass
+    return jsonify(results)
 
 
 if __name__ == "__main__":
